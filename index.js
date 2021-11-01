64ded0124a11144e16aa627c3053961c96ebb427
amiruljack <amirularifar@gmail.com>
2021-11-01 11:19:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,89 +1,86 @@
-'use strict'
-
-const signer = require('./signer')
-const Wreck = require('wreck')
-let wreck
+/* eslint-disable camelcase */
+/* eslint-disable require-jsdoc */
+const signer = require("./signer");
+const Wreck = require("wreck");
+let wreck;
 
 class Billplz {
-
   constructor(options) {
+    this._apiEndpoint = "https://www.billplz.com/api/v3/";
+    this._sandboxApiEndpoint = "https://www.billplz-sandbox.com/api/v3/";
 
-    
-    this._apiEndpoint = 'https://www.billplz.com/api/v3/'
-    this._sandboxApiEndpoint = 'https://www.billplz-sandbox.com/api/v3/'
-    
-      this._apiKey = options.key
-      this._isSandbox = options.sandbox
-      this._xSignatureKey = options.xSignatureKey
+    this._apiKey = options.key;
+    this._isSandbox = options.sandbox;
+    this._xSignatureKey = options.xSignatureKey;
 
     if (this._isSandbox === true) {
-      this._apiEndpoint = this._sandboxApiEndpoint
+      this._apiEndpoint = this._sandboxApiEndpoint;
     }
 
-    wreck =  Wreck.defaults({
-      headers: { 'Authorization': 'Basic ' + new Buffer.from(this._apiKey).toString('base64') }
-    })
+    wreck = Wreck.defaults({
+      headers: {"Authorization": "Basic " + Buffer.from(this._apiKey).toString("base64")},
+    });
   }
 
   request(method, params, callback) {
     wreck.post(this._apiEndpoint + method, {
-        payload: params
-      }, (err, res, payload) => {
-        try{
-        callback(err, JSON.parse(payload.toString()))
-        }catch(e){
-          throw(`${e} ${payload.toString()}`);
-        }
-    })
+      payload: params,
+    }, (err, res, payload) => {
+      try {
+        callback(err, JSON.parse(payload.toString()));
+      } catch (e) {
+        throw (`${e} ${payload.toString()}`);
+      }
+    });
   }
 
-  //create collection
+  // create collection
   create_collection(params, callback) {
-    return this.request('collections', params, callback)
+    return this.request("collections", params, callback);
   }
 
-  //create open collection
+  // create open collection
   create_collectionOpen(params, callback) {
-    return this.request('open_collections', params, callback)
+    return this.request("open_collections", params, callback);
   }
 
-  //create bill
+  // create bill
   create_bill(params, callback) {
-    return this.request('bills', params, callback)
+    return this.request("bills", params, callback);
   }
 
-  //get bill
+  // get bill
   get_bill(billId, callback) {
-    wreck.get(this._apiEndpoint + 'bills/' + billId, (err, res, payload) => {
-      callback(err, JSON.parse(payload.toString()))
-    })
+    wreck.get(this._apiEndpoint + "bills/" + billId, (err, res, payload) => {
+      callback(err, JSON.parse(payload.toString()));
+    });
   }
 
-  //delete bill
+  // delete bill
   delete_bill(billId, callback) {
-    wreck.delete(this._apiEndpoint + 'bills/' + billId, (err, res, payload) => {
-      callback(err, JSON.parse(payload.toString()))
-    })
+    wreck.delete(this._apiEndpoint + "bills/" + billId, (err, res, payload) => {
+      callback(err, JSON.parse(payload.toString()));
+    });
   }
 
-  //change collection status
+  // change collection status
   change_collection_status(collectionId, status, callback) {
-    wreck.post(this._apiEndpoint + 'collections/' + collectionId + '/' + status, (err, res, payload) => {
-      callback(err, JSON.parse(payload.toString()))
-    })
+    wreck.post(this._apiEndpoint + "collections/" + collectionId + "/" + status, (err, res, payload) => {
+      callback(err, JSON.parse(payload.toString()));
+    });
   }
 
-  //registration check
+  // registration check
   registration_check(bankAccountNumber, callback) {
-    wreck.get(this._apiEndpoint + 'check/bank_account_number/' + bankAccountNumber, (err, res, payload) => {
-      callback(err, JSON.parse(payload.toString()))
-    })
+    wreck.get(this._apiEndpoint + "check/bank_account_number/" + bankAccountNumber, (err, res, payload) => {
+      callback(err, JSON.parse(payload.toString()));
+    });
   }
-  
+
   verifySignature(object, signature) {
-    const objectSignature = signer(object, this._xSignatureKey)
-    return objectSignature === signature
+    const objectSignature = signer(object, this._xSignatureKey);
+    return objectSignature === signature;
   }
 }
 
-module.exports = Billplz
+module.exports = Billplz;
